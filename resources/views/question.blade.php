ef05997d069348120fc6bc5962341c5c74321035
Kyle Kreinheder <kk566@njit.edu>
2019-05-14 23:57:46 -0400

    Heroku setup, no .idea

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -26,7 +26,11 @@
                     <div class="card-body">
                         @forelse($question->answers as $answer)
                             <div class="card">
-                                <div class="card-body">{{$answer->body}}</div>
+                                <div class="card-body">{{$answer->body}}
+                                    <button class="btn btn-success float-right mr-2">👍</button>
+
+                                    <button class="btn btn-danger float-right mr-2" >👎</button>
+                                </div>
                                 <div class="card-footer"><a class="btn btn-primary float-right"
                                                             href="{{ route('answer.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
                                         View
